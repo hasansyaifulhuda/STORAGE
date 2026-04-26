219e608fad22ba474b8487add5999e4e7847642e
hasansyaifulhuda <hasansyaifulhuda24@gmail.com>
2026-04-26 07:11:44 +0700

    Add files via upload

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'https://gxyidnmeqddweeoaanm.supabase.co'
-const supabaseKey = 'sb_publishable_LRTWgT4AoMGohvvac97Avg_W2sEZZPP'
+const supabaseUrl = "https://gxyidnmeqddweeooaanm.supabase.co"
+const supabaseKey = "sb_publishable_LRTWgT4AoMGohvvac97Avg_W2sEZZPP"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
